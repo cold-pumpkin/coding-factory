e1bc28ea1f4ff8eb6f5484571a86cb117bf13b83
cold-pumpkin <pch4623@gmail.com>
2023-04-02 15:55:57 +0900

    Splash Screen - StatelessWidget

---
+++ splash_screen/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello world!'),
+      ),
+    );
+  }
+}
