628373e90e59b4ac3d752d1dc262259e3d176094
cold-pumpkin <pch4623@gmail.com>
2023-04-02 18:16:58 +0900

    Webview - WebView 위젯 & AppBar

---
+++ webview/lib/screen/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text('Code Factory'),
+        centerTitle: true,
+      ),
+      body: const WebView(
+        initialUrl: 'https://blog.codefactory.ai',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
