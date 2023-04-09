c669093fcac35d69cbf123a11c6d7b8376a13027
cold-pumpkin <pch4623@gmail.com>
2023-04-09 15:01:17 +0900

    랜덤숫자 생성기 - Slider 위젯 사용하기

--- random_number/lib/screen/setting_screen.dart
+++ random_number/lib/screen/setting_screen.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:random_number/constant/color.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  double maxNumber = 10000;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('settings!')),
+    return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Row(
+                  children: maxNumber
+                      .toInt()
+                      .toString()
+                      .split('')
+                      .map(
+                        (e) => Image.asset(
+                          'asset/image/$e.png',
+                          width: 50,
+                          height: 70,
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+              Slider(
+                  value: maxNumber,
+                  min: 10000,
+                  max: 1000000,
+                  onChanged: (double val) {
+                    setState(() {
+                      maxNumber = val;
+                    });
+                  }),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: RED_COLOR,
+                ),
+                child: const Text('저장!'),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
