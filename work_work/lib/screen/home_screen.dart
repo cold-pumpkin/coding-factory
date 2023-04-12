dcccc8ef83bd8650da658ff6065a82eded6ff076
cold-pumpkin <pch4623@gmail.com>
2023-04-12 22:50:30 +0900

    오늘도 출근 - 구글지도 사용해보기

--- work_work/lib/screen/home_screen.dart
+++ work_work/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,10 +9,68 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static const LatLng companyLatLng = LatLng(
+    37.365667391806,
+    127.10806048253,
+  );
+  static const CameraPosition initialPosition = CameraPosition(
+    target: companyLatLng,
+    zoom: 15,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("HomeScreen")),
+    return Scaffold(
+      appBar: renderAppBar(),
+      body: Column(
+        children: const [
+          _CustomGoogleMap(initialPosition: initialPosition),
+          _WorkButton(),
+        ],
+      ),
+    );
+  }
+
+  AppBar renderAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: const Text(
+        '오늘도 출근',
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    );
+  }
+}
+
+class _WorkButton extends StatelessWidget {
+  const _WorkButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Expanded(
+      child: Text('출근'),
+    );
+  }
+}
+
+class _CustomGoogleMap extends StatelessWidget {
+  const _CustomGoogleMap({
+    required this.initialPosition,
+  });
+
+  final CameraPosition initialPosition;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: initialPosition,
+      ),
     );
   }
 }
