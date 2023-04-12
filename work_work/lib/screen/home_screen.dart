193cd0ef6bf369ee04f85f2caa19e60e0c6b7738
cold-pumpkin <pch4623@gmail.com>
2023-04-13 00:02:38 +0900

    오늘도 출근 - FutureBuilder 위젯 사용하기

--- work_work/lib/screen/home_screen.dart
+++ work_work/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -22,13 +23,53 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: renderAppBar(),
-      body: Column(
-        children: const [
-          _CustomGoogleMap(initialPosition: initialPosition),
-          _WorkButton(),
-        ],
+      body: FutureBuilder(
+        future:
+            checkPermission(), // future 함수의 connectionState가 변경될 떄 마다 builer 재실행
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.data == '위치 권한이 허가되었습니다.') {
+            return Column(
+              children: const [
+                _CustomGoogleMap(initialPosition: initialPosition),
+                _WorkButton(),
+              ],
+            );
+          }
+          return Center(
+            child: Text(snapshot.data!),
+          );
+        },
       ),
     );
+  }
+
+  Future<String> checkPermission() async {
+    // 위치 서비스 활성화 여부 확인
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!isLocationEnabled) {
+      return '위치 서비스를 활성화 해주세요.';
+    }
+
+    // 위치 권한 확인
+    LocationPermission checkPermission = await Geolocator.checkPermission();
+    if (checkPermission == LocationPermission.denied) {
+      checkPermission = await Geolocator.requestPermission();
+      if (checkPermission == LocationPermission.denied) {
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    if (checkPermission == LocationPermission.deniedForever) {
+      return '앱의 위치 권한을 셋팅에서 허가해주세요.';
+    }
+
+    return '위치 권한이 허가되었습니다.';
   }
 
   AppBar renderAppBar() {
