f837f9cd0a3ca71954ac9c2e4c965e49da8f5d6b
cold-pumpkin <pch4623@gmail.com>
2023-04-15 17:38:24 +0900

    캘린더 스케줄러 - 프로젝트 셋팅

---
+++ calender_scheduler/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:calender_scheduler/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      fontFamily: 'NotoSans',
+    ),
+    home: const HomeScreen(),
+  ));
+}
